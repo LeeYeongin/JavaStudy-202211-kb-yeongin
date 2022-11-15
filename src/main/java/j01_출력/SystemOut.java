843ba3bf493ac8addae7682164b84b12fde9c891
LeeYeongin <youngin5757@gmail.com>
2022-11-15 12:21:59 +0900

    챕터1 출력

---
+++ src/main/java/j01_출력/SystemOut.java
@@ -0,0 +1,14 @@
+package j01_출력;
+
+public class SystemOut {
+    public static void main(String[] args) {
+        System.out.print("이름: ");
+        System.out.println("이영인");
+        System.out.print("주소: ");
+        System.out.println("부산 동래구 사직동");
+        System.out.print("연락처: ");
+        System.out.println("010-5195-1766");
+        System.out.print("성별: ");
+        System.out.println("여성");
+    }
+}
