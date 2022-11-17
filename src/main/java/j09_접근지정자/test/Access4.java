4e8c4b24b6480d9510b54d0f53dfb8a5bcb95f7a
LeeYeongin <youngin5757@gmail.com>
2022-11-17 12:57:36 +0900

    챕터9 접근지정자: public, private

---
+++ src/main/java/j09_접근지정자/test/Access4.java
@@ -0,0 +1,11 @@
+package j09_접근지정자.test;
+
+import j09_접근지정자.Access1;
+
+public class Access4 extends Access1 {
+
+     void access1PrintName(){
+
+    }
+
+}
---
+++ src/main/java/j09_접근지정자/캡슐/Student.java
@@ -0,0 +1,28 @@
+package j09_접근지정자.캡슐;
+
+public class Student {
+    private String name;
+    private String gender;
+
+    public Student(){}
+
+    public Student(String name, String gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void printInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Gender: " + gender);
+    }
+
+
+}
