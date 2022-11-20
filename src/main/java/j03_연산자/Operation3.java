5b8de6e180deb81ab1643d48b7d2c6257355ad26
LeeYeongin <youngin5757@gmail.com>
2022-11-20 23:58:18 +0900

    챕터3 연산자: 산술연산자

---
+++ src/main/java/j03_연산자/Operation3.java
@@ -0,0 +1,16 @@
+package j03_연산자;
+
+public class Operation3 {
+    public static void main(String[] args) {
+
+        int a = 11;
+        int b = 2;
+
+        System.out.println("a + b = " + (a + b));   //덧셈
+        System.out.println("a - b = " + (a + b));   //뺼셈
+        System.out.println("a * b = " + (a * b));   //곱셈
+        System.out.println("a / b = " + (a / b));   //나눗셈의 몫
+        System.out.println("a % b = " + (a % b));   //나눗셈의 나머지
+
+    }
+}
