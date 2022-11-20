3b8878fdf433d403005726e69ebec0dc3ca72dd3
LeeYeongin <youngin5757@gmail.com>
2022-11-20 23:56:28 +0900

    챕터3 증감연산자 수정

--- src/main/java/j03_연산자/Operation2.java
+++ src/main/java/j03_연산자/Operation2.java
@@ -4,10 +4,21 @@ public class Operation2 {
     public static void main(String[] args) {
 
         int num = 10;
-        num = num + 1;
 
         System.out.println(num++);
         System.out.println(num);
+        /*
+            print(num++)은
+            1. print(num)
+            2. num = num + 1
+         */
+        System.out.println(++num);
+        System.out.println(num);
+        /*
+            print(++num)은
+            1. num = num + 1
+            2. print(num)
+         */
 
     }
 }
