9d4472218f52f1b298207f30a5def0c403cc67e5
LeeYeongin <youngin5757@gmail.com>
2022-11-20 23:57:52 +0900

    챕터6 반복 주석설명 추가

--- src/main/java/j06_반복/Loop1.java
+++ src/main/java/j06_반복/Loop1.java
@@ -5,7 +5,7 @@ public class Loop1 {
 
         int n = 10;
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){ // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
             System.out.print(i);
             if(i < n - 1) {
                 System.out.print(", ");
@@ -13,7 +13,7 @@ public class Loop1 {
         }
         System.out.println();
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){ // 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
             System.out.print(i + 10);
             if(i < n - 1){
                 System.out.print(", ");
@@ -21,7 +21,7 @@ public class Loop1 {
         }
         System.out.println();
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){ // 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
             System.out.print(n - i);
             if(i < n - 1){
                 System.out.print(", ");
@@ -29,7 +29,7 @@ public class Loop1 {
         }
         System.out.println();
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){ // 20, 19, 18, 17, 16, 15, 14, 13, 12, 11
             System.out.print(n - i + 10);
             if(i < n - 1){
                 System.out.print(", ");
@@ -38,8 +38,7 @@ public class Loop1 {
         System.out.println();
 
         for(int i = 0, j = n; i < n; i++, j--){
-            System.out.println(i);
-            System.out.println(j);
+            System.out.println("i: " + i + ", j: " + j);
         }
     }
 }
