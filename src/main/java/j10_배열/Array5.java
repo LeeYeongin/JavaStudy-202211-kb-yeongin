a40c06028fb47a673c5dcb25f9eb9b2d500f7bda
LeeYeongin <youngin5757@gmail.com>
2022-11-21 12:55:29 +0900

    챕터10 배열: 문제2

--- src/main/java/j10_배열/Array5.java
+++ src/main/java/j10_배열/Array5.java
@@ -1,6 +1,11 @@
 package j10_배열;
 
 public class Array5 {
+    //최소값을 구하는 function
+    public static int getMinNumber(int num1, int num2){
+        return num1 < num2 ? num1 : num2;
+    }
+
     public static void main(String[] args) {
         int[] nums1 = new int[] {1, 8, 9, 16, 17};
         int[] nums2 = new int[] {2, 7, 10, 15, 18};
@@ -8,32 +13,11 @@ public class Array5 {
         int[] nums4 = new int[] {4, 5, 12, 13, 20};
 
         int[] resultArray = new int[5];
-        int min = 0;
 
-        System.out.print("resultArray: ");
         for(int i =0; i < resultArray.length; i++){
-            if(nums1[i] < nums2[i]){
-                min = nums1[i];
-            }else{
-                min = nums2[i];
-            }
-            if(min > nums3[i]){
-                min = nums3[i];
-            }
-            if(min > nums4[i]){
-                min = nums4[i];
-            }
-
-            resultArray[i] = min;
-
-            if(i < resultArray.length - 1){
-                System.out.print(resultArray[i] + ", ");
-            }else{
-                System.out.print(resultArray[i]);
-            }
+            resultArray[i] = getMinNumber(getMinNumber(nums1[i], nums2[i]), getMinNumber(nums3[i], nums4[i]));
+            System.out.println(resultArray[i]);
         }
-
-
 
         /*
         배열들 중 동일 인덱스에 위치한 값들 중 최소값들만 뽑아서 새로운 배열을 만든다.
