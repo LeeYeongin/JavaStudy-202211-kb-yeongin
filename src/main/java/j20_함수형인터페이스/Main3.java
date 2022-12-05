069cfe002bf760198adba8e0a63e2cfd1fd49c19
LeeYeongin <youngin5757@gmail.com>
2022-12-05 12:52:22 +0900

    챕터20 함수형인터페이스: List, Map forEach

--- src/main/java/j20_함수형인터페이스/Main3.java
+++ src/main/java/j20_함수형인터페이스/Main3.java
@@ -1,7 +1,11 @@
 package j20_함수형인터페이스;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.function.Consumer;
 
 public class Main3 {
@@ -30,5 +34,33 @@ public class Main3 {
 		
 		forEachTest(n -> {System.out.println(n);}, names);
 		//n -> {System.out.println(n);} => Consumer의 accept메소드 override
+		
+		System.out.println();
+		Map<String, String> subject = new HashMap<>();
+		
+		subject.put("java", "자바");
+		subject.put("python", "파이썬");
+		subject.put("javascript", "자바스크립트");
+		subject.put("C", "씨언어");
+		subject.put("Node.js", "노드제이에스");
+		
+		Set<String> keySet = subject.keySet();
+		keySet.forEach(key -> {
+			
+			System.out.println(subject.get(key));
+		});
+		
+		System.out.println();
+		
+		Set<Entry<String, String>> entrySet = subject.entrySet();
+		entrySet.forEach(entry -> {
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+		});
+		
+		System.out.println();
+		
+		subject.forEach((key, value) -> {
+			System.out.println(key + ": " + value);
+		});
 	}
 }
